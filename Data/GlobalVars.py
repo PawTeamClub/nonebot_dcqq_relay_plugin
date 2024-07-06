f18ef1ad4a1774c7aa2afdc54c7239533e98be6b
Robonyantame <107612561+Robonyantame@users.noreply.github.com>
2024-07-06 21:18:49 +0800

    Update: 优化Setup方法

--- Data/GlobalVars.py
+++ Data/GlobalVars.py
@@ -1,9 +1,9 @@
+from pathlib import Path
 from nonebot import on_message, on_notice
 from nonebot.adapters.discord import Bot as DiscordBot
 from nonebot.adapters.onebot.v11 import Bot as OneBotBot
-from pathlib import Path
 
-BOT_NAME = "nonebot_dcqq_bot";
+#======================================================
 
 # 初始化 NoneBot
 # 给初始化用的
@@ -11,12 +11,14 @@ OneBotBotObj: OneBotBot = None;
 DiscordBotObj: DiscordBot = None;
 webhook_id = None;
 webhook = None;
+
+#======================================================
+
+# 常量
+BOT_NAME = "nonebot_dcqq_bot";
 DOWNLOAD_PATH: Path = None;
 
-# 配置
-# 除了webhook名字其他都会塞进env进行配置
-DISCORD_CHANNEL_ID = "1123601828794343536";
-QQ_GROUP_ID = "138889370";
+#======================================================
 
 # 创建事件处理器
 messageEvent = on_message(priority=10, block=True);
--- Setup.py
+++ Setup.py
@@ -1,38 +1,33 @@
-from .Data import GlobalVars
-from .Data.IConfig import plugin_config;
-from nonebot import get_bots
-from nonebot.log import logger
-from nonebot_plugin_apscheduler import scheduler
 from pathlib import Path
+from .Data import GlobalVars
+from nonebot import get_driver
+from nonebot.log import logger
+from .Data.IConfig import plugin_config;
+from nonebot.adapters.discord import Bot as DiscordBot
+from nonebot.adapters.onebot.v11 import Bot as OneBotBot
 
-# 加载 OneBot 适配器
-# 由于nonebot_discord_adapter的特殊性，他要使用心跳包接受bot的信息才能正常使用，所以我使用了计时器来让discord bot正常获取
-# 在思考要不要把这里丢进消息事件中
-# 这里应该有更好的优化方式，但是我真的不是很了解nonebot这玩意
-@scheduler.scheduled_job("interval", seconds=5, id="job_0")
-async def init():
-    
-    # 获取所有bot
-    bots = get_bots();
+# 以下大部分代码是对着此项目改的: https://github.com/Autuamn/nonebot-plugin-dcqg-relay/blob/main/nonebot_plugin_dcqg_relay/__init__.py
+driver = get_driver()
 
-    # 如果加载不到bot列表，那就直接中断
-    # 还是因为不知道适配器是否有相应时间的问题
-    if not bool(bots):
-        return False;
+path = Path() / "data" / "download";
+if not path.exists():
+    path.mkdir(parents=True, exist_ok=True);
+GlobalVars.DOWNLOAD_PATH = path;
 
-    GlobalVars.DiscordBotObj = next((bot for bot in bots.values() if bot.type == "Discord"), None);
-    if not GlobalVars.DiscordBotObj:
-        logger.warning("未找到所需的Discord机器人，将在下次执行时重试");
+@driver.on_bot_connect
+async def get_dc_bot(bot: DiscordBot):
+    GlobalVars.DiscordBotObj = bot
+
+@driver.on_bot_connect
+async def get_qq_bot(bot: OneBotBot):
+     GlobalVars.OneBotBotObj = bot
+
+@driver.on_bot_connect
+async def prepare_webhooks(bot: DiscordBot):
+    if not bot:
         return;
 
-    GlobalVars.OneBotBotObj = next((bot for bot in bots.values() if bot.type == "OneBot V11"), None);
-    if not GlobalVars.OneBotBotObj:
-        logger.warning("未找到所需的OneBot机器人，将在下次执行时重试");
-        return;
-    
-    # 通过频道id查询webhook列表
-    # 如果没有创建那就创建，如果有那就直接用
-    webhooks = await GlobalVars.DiscordBotObj.get_channel_webhooks(channel_id=int(plugin_config.discord_channel));
+    webhooks = await bot.get_channel_webhooks(channel_id=int(plugin_config.discord_channel));
     webhookTemp = next((w for w in webhooks if w.name == GlobalVars.BOT_NAME), None);
     if bool(webhookTemp): 
         logger.debug("寻找到Webhook");
@@ -40,14 +35,5 @@ async def init():
         GlobalVars.webhook_id = webhookTemp.id;
     else:
         logger.debug("没有寻找到Webhook, 正在创建");
-        GlobalVars.webhook = await GlobalVars.DiscordBotObj.create_webhook(channel_id=int(plugin_config.discord_channel), name=GlobalVars.BOT_NAME);
+        GlobalVars.webhook = await bot.create_webhook(channel_id=int(plugin_config.discord_channel), name=GlobalVars.BOT_NAME);
         GlobalVars.webhook_id = GlobalVars.webhook.id;
-
-    path = Path() / "data" / "download";
-    if not path.exists():
-        path.mkdir(parents=True, exist_ok=True);
-    GlobalVars.DOWNLOAD_PATH = path;
-
-    # 关闭计时器
-    scheduler.shutdown();
-
